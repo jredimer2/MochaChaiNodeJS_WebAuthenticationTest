071754231317786d3f897c523b9fe50092946690
jredimer <jredimer@yahoo.com.au>
2021-11-02 10:41:11 +1100

    Joseph Redimerio interview test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express()
 const port = 3000
+var base64 = require('base-64');
+var utf8 = require('utf8');
 
 app.use(express.json())
 
@@ -9,9 +11,34 @@ app.head('/status', (req, res) => { res.status(200).end(); });
 
 const basicAuthHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
   // implementation here
-  
+  console.log('testpoint 1');
+  console.log(req.headers.authorization);
+
+  let username = 'matt@gmail.com';
+  let password = 'this is a v@lid password!';
+
+  var text = username + ':' + password;
+  var bytes = utf8.encode(text);
+  var encoded = 'Basic ' + base64.encode(bytes);
+
+  console.log('encoded =', 'Basic ' + encoded);
+
+
+
+  //let auth : string [] = req.headers.authorization?.split(' ');
   
   // authentication succesful, pass request onto next middleware
+  console.log(`[${req.headers.authorization}]`);
+  console.log(`[${encoded}]`);
+
+  if (encoded == req.headers.authorization) 
+    console.log('Authenticated');
+  else 
+    console.log('Not authenticated');
+
+  var decodedData = base64.decode("");
+
+  console.log(decodedData);
   next()
 }
 
