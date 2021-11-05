5746cc8b420d8901b3fe490b7f5cd75d49eba860
jredimer <jredimer@yahoo.com.au>
2021-11-05 18:37:31 +1100

    added authentication checks, and Chai tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -19,12 +19,54 @@ describe('express', function () {
       .expect(404, done);
   });
 
-  describe('basic auth', async() => {
+  describe('basic auth', async () => {
     it('should respond with 200 when called with valid Authorization header value', async () => {
       const res = await request(server)
-      .get('/basic-auth')
-      .set('authorization', 'Basic bWF0dEBnbWFpbC5jb206dGhpcyBpcyBhIHZAbGlkIHBhc3N3b3JkIQ==');
-      expect(res.statusCode).to.equal(200);
+        .get('/basic-auth')
+        .set('authorization', 'Basic bWF0dEBnbWFpbC5jb206dGhpcyBpcyBhIHZAbGlkIHBhc3N3b3JkIQ==');
+        expect(res.statusCode).to.equal(200);      
     });
+
+    it('should respond with 401, "Invalid key" when called with invalid Authorization header value', async () => {
+      const res = await request(server)
+        .get('/basic-auth')
+        .set('authorization', 'Basic bWF0dEBnbWFpbC5jb206dGhpcyBpcyBhIHZAbGlkIHBhc3N3b3JkIQ==Invalid');
+        expect(res.statusCode).to.equal(401);
+        expect(res.text).to.equal('Invalid key');
+    });
+
+    it('should respond with 401, "Missing Basic prefix" when called Authorization header without the Basic prefix', async () => {
+      const res = await request(server)
+        .get('/basic-auth')
+        .set('authorization', 'bWF0dEBnbWFpbC5jb206dGhpcyBpcyBhIHZAbGlkIHBhc3N3b3JkIQ==');
+        expect(res.statusCode).to.equal(401);
+        expect(res.text).to.equal('Missing Basic prefix');
+    });
+
+    it('should respond with 401, "Missing username" when called Authorization header where username is missing', async () => {
+      const res = await request(server)
+        .get('/basic-auth')
+        .set('authorization', 'Basic OnRoaXMgaXMgYSB2QGxpZCBwYXNzd29yZCE=');
+        expect(res.statusCode).to.equal(401);
+        expect(res.text).to.equal('Missing username');
+    });
+
+    it('should respond with 401, "Missing password" when called Authorization header where password is missing', async () => {
+      const res = await request(server)
+        .get('/basic-auth')
+        .set('authorization', 'Basic bWF0dEBnbWFpbC5jb206');
+        expect(res.statusCode).to.equal(401);
+        expect(res.text).to.equal('Missing password');
+    });
+
+    it('should respond with 401, "Username/password connot contain :" when called where username/password contains a colon', async () => {
+      const res = await request(server)
+        .get('/basic-auth')
+        .set('authorization', 'Basic bWF0dEBnbWFpOmwuY29tOnRoaXMgaXMgYSB2QGxpZCBwYXNzd29yZCE=');
+        expect(res.statusCode).to.equal(401);
+        expect(res.text).to.equal('Username/password connot contain :');
+    });
+
   })
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,77 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
+import base64 from 'base-64';
+import utf8 from 'utf8';
+import { VALID_USERS } from './userLogins';
+
 const app = express()
 const port = 3000
+
+
+class Error401 extends Error {
+  status: number;
+  message: string;
+  constructor(message: string) {
+    super(message);
+    this.status = 401;
+    this.message = message;
+  }
+}
 
 app.use(express.json())
 
 app.get('/status', (req, res) => { res.status(200).end(); });
 app.head('/status', (req, res) => { res.status(200).end(); });
 
+//
+// Description: I authenticate username and passowrd using base64. 
+// If it matches the header authorization key, then I call the next() middleware. 
+// Otherwise, I call next(Error401), forcing the errorMiddleware() to be called and return status 401.
+//
 const basicAuthHandler = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+
   // implementation here
+  let userId = 1;  // select VALID_USER to test
+  let { userLogin, password } = VALID_USERS.users[userId];
   
-  
+  let auth : string = req.headers.authorization || '';
+  var authArr = auth.split(' ');
+
+  try {
+    var regExp = new RegExp('^Basic ');
+    if (!regExp.test(auth))
+      return next(new Error401('Missing Basic prefix'));
+
+    var decodedBase64 = base64.decode(authArr[1]); // throws exception if invalid authArr[1] value 
+    var credArr = decodedBase64.split(':');
+
+    if (credArr.length > 2)
+      return next(new Error401('Username/password connot contain :'));
+
+    if (credArr[0] == '')
+      return next(new Error401('Missing username'));
+
+    if (credArr[1] == '')
+      return next(new Error401('Missing password'));
+
+    if (credArr[0] != userLogin || credArr[1] != password)
+      return next(new Error401('Invalid userLogin or password'));
+
+  } catch (error) {
+    return next(new Error401('Invalid key'));
+  }
+
   // authentication succesful, pass request onto next middleware
   next()
+
 }
 
 app.get('/basic-auth', basicAuthHandler, (req: express.Request, res: express.Response) => {
   res.status(200).end();
+})
+
+app.use(function errorMiddleware(err: Error401, req: express.Request, res: express.Response, next: NextFunction) {
+  if (err instanceof Error401)
+    res.status(err.status).send(err.message);
 })
 
 export const server = app.listen(port, () => {
